fee222ff3e6f59c2a64e4e9b04ba9c5fb3cce8f2
TotoMC-13 <tomasazcaray@gmail.com>
2024-09-10 21:20:25 -0300

    ej 10 a b y casi c

---
+++ Practica 4/ej10.hs
@@ -0,0 +1,12 @@
+f1 :: Integer -> Integer
+f1 n | n == 0 = 1
+     | otherwise = f1(n-1) + 2^n
+
+f2 :: Integer -> Integer -> Integer
+f2 n q | n == 1 = q^n
+       | otherwise = f2(n-1) q + q^n
+
+f3 :: Integer -> Integer -> Integer
+f3 n q | n == 0 = 0
+       | n == 1 = q^n
+       | otherwise = f3((2*n)-1) q + q^n
