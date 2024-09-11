9f49e6f382243c2d80716d99cd4ce0003101c20e
Estudiante <jmnahas>
2024-09-11 09:18:10 -0300

    ej 10 c

--- Practica 4/ej10.hs
+++ Practica 4/ej10.hs
@@ -8,5 +8,9 @@ f2 n q | n == 1 = q^n
 
 f3 :: Integer -> Integer -> Integer
 f3 n q | n == 0 = 0
-       | n == 1 = q^n
-       | otherwise = f3((2*n)-1) q + q^n
+       | n == 1 = q
+       | otherwise = g(2*n) q
+       where
+       g :: Integer -> Integer -> Integer
+       g n q | n == 1 = q
+             | otherwise = g(n-1) q + q^n
