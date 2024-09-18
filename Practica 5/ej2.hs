ca5d575a514a41380e813de7d4ab7d8fc34c32d2
Estudiante <Estudiante@pc15.lab1106.labos.fcen.uba.ar>
2024-09-18 13:49:34 -0300

    ej2 1

--- Practica 5/ej2.hs
+++ Practica 5/ej2.hs
@@ -1,3 +1,10 @@
+-- 1
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece _ [] = False
+pertenece n (x:xs) 
+                | n == x = True
+                | otherwise = pertenece n xs
+
 -- 5
 quitar :: (Eq t) => t -> [t] -> [t]
 quitar _ [] = []
