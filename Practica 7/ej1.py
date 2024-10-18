eda54c838579b7c1001c1641bfd96b049f981433
TotoMC-13 <tomasazcaray@gmail.com>
2024-10-18 00:24:54 -0300

    auto push script

--- Practica 7/ej1.py
+++ Practica 7/ej1.py
@@ -51,3 +51,64 @@ def minimo(numeros: list) -> int:
             minimo_numero = i
 
     return minimo_numero
+
+def ordenados(numeros: list) -> bool:
+    anterior: int = numeros[0]
+
+    for i in numeros:
+        if i < anterior:
+            return False
+        anterior = i
+
+    return True
+
+def pos_maximo(numeros: list) -> int:
+    maximo_numero: int = numeros[0]
+    posicion: int = -1
+    posicion_maximo: int = 0
+    
+    for i in numeros:
+        posicion += 1
+
+        if i > maximo_numero:
+            maximo_numero = i
+            posicion_maximo = posicion
+
+    return posicion_maximo
+
+def pos_minimo(numeros: list) -> int:
+    minimo_numero: int = numeros[0]
+    posicion: int = -1
+    posicion_minimo: int = 0
+    
+    for i in numeros:
+        posicion += 1
+
+        if i < minimo_numero:
+            minimo_numero = i
+            posicion_minimo = posicion
+
+    return posicion_minimo
+
+def palabra_7_o_mas(palabras: list) -> bool:
+    for i in palabras:
+        if len(i) >= 7:
+            return True
+    
+    return False
+
+def es_palindromo(palabra: str) -> bool:
+    return palabra == palabra[::-1]
+
+def tres_seguidos(numeros: list) -> bool:
+    longitud = len(numeros)
+    
+    for i in range(longitud):
+
+        if i <= longitud - 3 and numeros[i] == numeros[i + 1] == numeros[i + 2]:
+            return True
+
+        if i >= 2 and numeros[i] == numeros[i - 1] == numeros[i - 2]:
+            return True
+            
+    return False
