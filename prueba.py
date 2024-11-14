b7b7ca1bfc0bed6664d8c19ece8cee0ef4a1a477
TotoMC-13 <tomasazcaray@gmail.com>
2024-11-14 12:56:17 -0300

    auto push script

--- prueba.py
+++
@@ -1,9 +0,0 @@
-def agregar_producto(inventario: dict[str, dict[str, float ,int]], nombre: str, precio: float, cantidad: int):
-    stockInfo:dict[str, float | int] = inventario[nombre]
-    stockInfo["precio"] = precio
-    stockInfo["cantidad"] = cantidad
-
-stock: dict[str, dict[str, float | int]] = {} 
-
-agregar_producto(stock, "manzana", 10.99, 12)
-print(stock)
